f63aa006c7e2431498276e56bc1a9eaa40218335
hotmailbelike <fishilike.fish@gmail.com>
2019-12-15 13:55:43 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export default App;
+
+const name = 'Josh Perez';
+const element = (
+	<div>
+		<h1>Hello, {name}</h1> <h1>Hello, {name}</h1>
+	</div>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return element;
 }
 
-export default App;
+// export default element;
+
+/*
+  Module 2 done in index.js
+*/
